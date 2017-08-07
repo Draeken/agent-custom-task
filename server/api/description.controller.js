008587bfe68a2a1d46982a7a59b496f037f40c8e
Draeken <pbm764927@gmail.com>
2017-08-07 16:46:20 +0200

    Fix description controller

--- server/api/description.controller.js
+++ server/api/description.controller.js
@@ -2,13 +2,15 @@ var http = require('http');
 var helper = require('./helper');
 
 function computeDescription(user, recipeId) {
-  return user.recipeInfos.id(recipeId).recipe.description;
+  return user.recipeInfos
+    .find(recipeInf => recipeInf.queries.some(q => q.taskIdentity.id == recipeId))
+    .recipe.description;
 }
 
 module.exports = (options) => {
   return (req, res, next) => {
     helper.retrieveUser(req.body.token, options.autoSchedule)
-      .then(user => computeDescription(user, req.param.id))
+      .then(user => computeDescription(user, req.params.taskId))
       .then(desc => res.json({ description: desc }))
       .catch(err => next(err));
   }
